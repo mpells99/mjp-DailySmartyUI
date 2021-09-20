c69bb409729e7d1d76cbf82d82fe5ae62011d8a6
MJ Pells <mcdonaldjpells@gmail.com>
2021-09-20 14:56:12 -0600

    used axios to perform get request to fetch recents posts and setup the required reducer, and fixed an index.js file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,12 +5,3 @@ import { reducer as form } from "redux-form";
 const rootReducer = combineReducers({
   form,
 });
-
-export default rootReducer;
-
-export function fetchRecentPosts() {
-  return function (dispatch) {
-    //perform our request in here.
-    console.log("hello");
-  };
-}
